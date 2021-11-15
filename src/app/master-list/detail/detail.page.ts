c14754ea765b52bfe7d1633995c49907d43ba9d0
Lewis Lee <lewisforbusiness@gmail.com>
2021-11-15 17:07:08 -0500

    feat|core|display details of selected person parse the route param and filter list items to find specific person and display details of that person in detail.page.html

--- src/app/master-list/detail/detail.page.ts
+++ src/app/master-list/detail/detail.page.ts
@@ -1,4 +1,7 @@
+import { MasterListItemService } from './../master-list-item.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { parseI18nMeta } from '@angular/compiler/src/render3/view/i18n/meta';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+  chosenItem;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private masterListItemService: MasterListItemService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      const chosenName = paramMap.get('listItemName');
+      console.log('Chosen Name: ', chosenName);
+      this.masterListItemService.getItems()
+      // .subscribe((item: any)=> console.log('item are:', item.results))
+      .subscribe((items: any) => this.chosenItem = items.results.find(item => item.name === chosenName));
+    });
   }
 
 }
